d8f0b80501cd4dd62e5b380393fbcdc3cb198545
Ruchika Joshi <ruchika_joshi@epam.com>
2026-09-07 12:27:58 +0530

    chore: align root directory to only keep .github and .vscode configurations

--- tests/integration/test_external_dependencies.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import pytest
-
-
-@pytest.mark.integration
-@pytest.mark.requires_env
-def test_external_dependencies_reachable(require_external_dependencies: None) -> None:
-    assert require_external_dependencies is None
